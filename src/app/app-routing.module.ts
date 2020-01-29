db5122df10bbfe14e3fc2afb95fc9a41471438d8
Unknown <heintz.gilles@gmail.com>
2020-01-29 17:31:52 +0100

    :construction: routing & toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {LoginComponent} from './components/login/login.component';
+import {NewsComponent} from './components/news/news.component';
+import {TrombiComponent} from './components/trombi/trombi.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'trombinoscope', component: TrombiComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
