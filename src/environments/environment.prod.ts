c27317aceb4ec81f4193871971beefd12feabaca
Gilles <heintz.gilles@gmail.com>
2020-03-04 12:02:50 +0100

    :rocket: Setup firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+export const APIKEY = 'AIzaSyAJ2M7YaYOv2iTOB8PUa1_osEmfLogWgrU';
+export const PROJECTID = 'portail-web-7c9e4';
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: APIKEY,
+    authDomain: `${PROJECTID}.firebaseapp.com`,
+    databaseURL: `https://${PROJECTID}.firebaseio.com`,
+    projectId: PROJECTID,
+    storageBucket: `${PROJECTID}.appspot.com`
+  }
 };
